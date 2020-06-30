e3435ccc32769d8a615562a97cda3d327a0845a8
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-06-30 07:29:23 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -21,6 +21,9 @@ class RecipesController < ApplicationController
   private
   
   def recipe_params
-    params.require(:recipe).permit
+    params.require(:recipe).permit(:title, ingredients_attributes: [
+      :name,
+      :quantity
+      ])
   end
 end
